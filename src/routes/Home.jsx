cf5b79c3901f9145defecb1acf71299f98f82f8c
Deathfab <chiarandini.christopher@gmail.com>
2023-04-13 16:32:05 +0200

    modification de la mise en page de routes/Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,14 @@
-import "../styles/Home.css"
-import logo from "../assets/logo-OP.png"
+import React from 'react'
+import '../styles/Home.css'
+import logo from '../assets/logo-OP.png'
 
 export default function Home() {
-
-return(
+  return (
     <div className="container">
-        <img src={logo} alt="logo one piece"></img>
-        <div>Bienvenue dans le monde de One Piece</div>
+      <img src={logo} alt="logo one piece"></img>
+      <div>Bienvenue dans le monde de One Piece</div>
 
-        <a href="/Tomes">Les Tomes de One Piece</a>
+      <a href="/Tomes">Les Tomes de One Piece</a>
     </div>
-)
-
+  )
 }
