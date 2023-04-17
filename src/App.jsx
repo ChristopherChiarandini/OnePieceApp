607ee999fc2fadccd9debcd33325febd77bcd8bd
Deathfab <chiarandini.christopher@gmail.com>
2023-04-17 14:09:40 +0200

    feat: update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './styles/App.css'
 import React from 'react'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home'
 import IndexTome from './routes/Tomes'
 import IndexChap from './routes/Chapitres'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
