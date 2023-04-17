bff8f1ee7a1784995f2aa2e165a52b5c525e816f
Deathfab <chiarandini.christopher@gmail.com>
2023-04-17 13:14:04 +0200

    feat: upgrade for gh-pages

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../styles/Home.css'
-import logo from '../assets/logo-OP.png'
+// import logo from '../assets/logo-OP.png'
 
 export default function Home() {
   return (
     <div className="container">
-      <img src={logo} alt="logo one piece"></img>
-      <div>Bienvenue dans le monde de One Piece</div>
-
-      <a href="/tomes">Les Tomes de One Piece</a>
+      <div className="box">
+        {/* <img src={logo} alt="logo one piece"></img> */}
+        <p>Bienvenue dans le monde de One Piece</p>
+        <a href="/tomes">Les Tomes de One Piece</a>
+      </div>
+      <div></div>
     </div>
   )
 }
