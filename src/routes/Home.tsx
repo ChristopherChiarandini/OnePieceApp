a4b6e5bbbe5ea50044b261c958682d03dda3200a
Deathfab <chiarandini.christopher@gmail.com>
2023-04-18 10:49:14 +0200

    Revert "link to"
    This reverts commit 857e52e7c773f17fe2270189cf198840482f1b62.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="box">
         {/* <img src={logo} alt="logo one piece"></img> */}
         <p>Bienvenue dans le monde de One Piece</p>
-        <Link to="/tomes">Les Tomes de One Piece</Link>
+        <Link href="/tomes">Les Tomes de One Piece</Link>
       </div>
       <div></div>
     </div>
